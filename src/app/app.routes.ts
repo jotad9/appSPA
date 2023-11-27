9728a0c1197c347e6549cf83e397db42eb6b3111
jotad9 <jotadaniel2003@gmail.com>
2023-11-27 19:47:50 +0100

    modifico app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,40 @@
 import { Routes } from '@angular/router';
 import { AboutPageComponent } from './components/aboutPage/aboutPage.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { CountriesComponent } from './countries/countries.component';
+import { ByCapitalPageComponent } from './countries/pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './countries/pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './countries/pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './countries/pages/country-page/country-page.component';
 
 
 //configuracion de rutas
 export const routes: Routes = [
+
   {
-    path:'',
-    component: HomePageComponent
-  }, {
     path:'about',
     component: AboutPageComponent
   }, {
     path:'contact',
     component: ContactComponent
   }, {
+    path:'countries',
+    component: CountriesComponent,
+    children: [{
+      path:'by-capital',
+      component: ByCapitalPageComponent
+      }, {
+      path:'by-country',
+      component: ByCountryPageComponent
+      }, {
+      path:'by-region',
+      component:ByRegionPageComponent
+      }, {
+      path: 'by/:id',
+      component: CountryPageComponent
+      }]
+  }, {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'countries'
   }
 ];
--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -2,11 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 @Component({
   selector: 'app-countries',
   standalone: true,
-  imports: [CommonModule, RouterModule, ByCapitalPageComponent,],
+  imports: [CommonModule,
+            RouterModule,
+            ByCapitalPageComponent,
+            ByCountryPageComponent,
+            ByRegionPageComponent,
+            CountryPageComponent
+          ],
   templateUrl: './countries.component.html',
   styles: ''
 })
